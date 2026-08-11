17f58241eacdc6b714c642f953f5c7d60a7ce3cf
Yogesh <nitya.upmanyu@gmail.com>
2026-08-11 21:42:51 +0530

    fix some issues

--- backend/src/config/cloudinary.ts
+++ backend/src/config/cloudinary.ts
@@ -4,7 +4,7 @@ import "dotenv/config";
 cloudinary.config({
     cloud_name:process.env.CLODINARY_CLOUD_NAME!,
     api_key:process.env.CLOUDINARY_API_KEY!,
-    api_secret:process.env.COUDINARY_API_SECRET!,
+    api_secret:process.env.CLOUDINARY_API_SECRET!,
     secure:true
 })
 
